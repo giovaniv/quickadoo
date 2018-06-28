d38352a1d3a3d17d4b36b1c7ce28df3366ac47fa
byeong0430 <byeong.kim0430@gmail.com>
2018-06-28 20:40:01 +0000

    added post /events and wrote some test code to see if `post` works

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const { checkRecord, insertRecords } = require('../libs/query-helpers');
 
-module.exports = (knex) => {
+module.exports = knex => {
 
   router.get("/", (req, res) => {
     knex
@@ -11,8 +12,27 @@ module.exports = (knex) => {
       .from("users")
       .then((results) => {
         res.json(results);
-    });
+      });
   });
+
+  // user completes and submit the event and user forms
+  // use post() for now for debugging but change it to put()
+  router.post('/events', (req, res) => {
+    const table = 'users';
+    // test run
+    // const { name } = req.body;
+    checkRecord(knex, req.body, table)
+      .then(() => {
+        // record doesn't exist. insert the data
+        insertRecords(knex, table, req.body)
+          .then(result => console.log(result))
+          .catch(err => console.log(err));
+      })
+      .catch(err => {
+        // duplicate exists. print out an error
+        if (err) console.log(err.message, err.record);
+      });
+  })
 
   return router;
 }
