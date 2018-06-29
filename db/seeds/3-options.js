672ffa49d20a696806d48548383561fbc667c2cb
Wisara8 <srandall845@gmail.com>
2018-06-29 18:49:11 +0000

    updated options seed

--- db/seeds/3-options.js
+++ db/seeds/3-options.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('options').del()
     .then(function () {
       return Promise.all([
-        knex('options').insert({title: 'Avengers', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'}),
-        knex('options').insert({title: 'Avengers', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'}),
-        knex('options').insert({title: 'Batman', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'})
+        knex('options').insert({name: 'Avengers', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'}),
+        knex('options').insert({name: 'Avengers', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'}),
+        knex('options').insert({name: 'Batman', start_time: '2018.09.09', end_time: '2018.09.09', note: 'pick me!'})
       ]);
     });
 };
