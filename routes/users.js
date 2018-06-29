e6c8a80f4b8f0857a7c371cf55d1480ba2f17caa
byeong0430 <byeong.kim0430@gmail.com>
2018-06-29 23:02:35 +0000

    working on get /events/:event_id route

--- routes/users.js
+++ routes/users.js
@@ -38,8 +38,8 @@ module.exports = knex => {
           });
         }
         // if event_id === admin_url, usere is an admin.
-        // Render event.ejs for the admin
-        if (rows.admin_url === event_id) {
+        // render event.ejs for the admin
+        if (rows[0].admin_url === event_id) {
           res.status(200).render('event');
         } else {
           // event_id === poll_url. Render event.ejs for attendees
@@ -78,42 +78,47 @@ module.exports = knex => {
     // options
     const option = {
       'name': req.body['name-1'],
-      'start_time': req.body['start_time-1'],
-      'end_time': req.body['end_time-1'],
+      'start_time': req.body['start_time-1'] ? req.body['start_time-1'] : null,
+      'end_time': req.body['end_time-1'] ? req.body['end_time-1'] : null,
       'note': req.body['note-1']
     };
 
-    // check user record
+    // 1. check user record
     checkRecord(knex, user, 'users')
-      // user doesn't exist in users tb
+      // if user doesn't exist in users tb, add the new user
       .then(() => {
-        console.log('not exist');
+        console.log("user doesn't exist in db. adding user...");
+        return insertRecords(knex, 'users', user);
       })
-      // user exists in db. retrieve the user_id
+      // if user exists in db. retrieve the user_id
       .catch(duplicate => {
         console.log('user exists in db. retrieving user_id');
         return duplicate.record[0].id;
       })
-      // use the user_id to insert the event record in events tb
+      // insert the user_id & the event record in events tb
       .then(creator_id => {
+        console.log('user added!');
         event.creator_id = creator_id;
-        // insert the event data along with the user id
         return insertRecords(knex, 'events', event)
       })
-      .catch(err => console.log(err.message))
-      // return the event_id and insert option data with it
+      // upon success, append the new event row's unique id (event_id) to the options object
+      // then insert the options data into options tb
       .then(result => {
         console.log(result.message);
-
         option.event_id = result.rowId[0];
         return insertRecords(knex, 'options', option);
       })
-      // print out any error while inserting option data
+      // if inserting event record was UNSUCCESSFUL, display error message
+      .catch(err => console.log(err.message))
+      // if options were added to options tb, display a success message
       .then(result => {
-        console.log('1111111111111111111111');
+        console.log(result.message);
+        // redirect user to the admin page!
+        const adminPage = `/events/${event.admin_url}`;
+        res.redirect(adminPage);
       })
+      // if inserting options was UNSUCCESSFUL, display error message
       .catch(err => console.log(err));
-
   })
 
 
