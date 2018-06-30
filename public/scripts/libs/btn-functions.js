1b04c73c6385720aad1444562f6d1243efaba82e
byeong0430 <byeong.kim0430@gmail.com>
2018-06-30 23:18:41 +0000

    added url-copy button function

--- public/scripts/libs/btn-functions.js
+++ public/scripts/libs/btn-functions.js
@@ -15,7 +15,21 @@ const activateDeleteBtn = () => {
   })
 };
 
+const makeUrlCopyBtn = () => {
+  $('article.nav-alert-wrapper .input-group').click(function () {
+    const $parentDiv = $(this).parents().eq(1);
+    // get the input value and copy it to clipboard
+    const $value = $parentDiv.find('input[type="text"]').val();
+    const $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($value).select();
+    document.execCommand("copy");
+    $temp.remove();
+  })
+};
+
 window.activateBtns = {
   activateCopyBtn,
-  activateDeleteBtn
+  activateDeleteBtn,
+  makeUrlCopyBtn
 }
