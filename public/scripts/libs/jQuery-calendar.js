2aa478a08b5dc0c8babb0d2356cff9f45a8a1472
byeong0430 <byeong.kim0430@gmail.com>
2018-07-01 07:10:11 +0000

    added files required for jQuery-calendar

--- public/scripts/libs/jQuery-calendar.js
+++ public/scripts/libs/jQuery-calendar.js
@@ -1,19 +1,2 @@
-jQuery.datetimepicker.setLocale('en');
-
-jQuery('#datetimepicker').datetimepicker({
-  i18n:{
-   en:{
-    months:[
-     'January','February','Mars','April',
-     'May','Jun', 'July','August',
-     'September','October','November','Deember',
-    ],
-    dayOfWeek:[
-     "Su.", "Mo", "Tues", "Wed",
-     "Thurs", "Fri", "Sat",
-    ]
-   }
-  },
-  timepicker:false,
-  format:'d.m.Y'
- });
+$('#datetimepicker1').datetimepicker();
+$('#datetimepicker2').datetimepicker();
