185ef27e545df8e23b17d1366a5cd79150ed3db1
byeong0430 <byeong.kim0430@gmail.com>
2018-07-01 05:40:42 +0000

    refactored form-functions.js. still working on ajax error handling in submitEventForm()

--- public/scripts/libs/form-functions.js
+++ public/scripts/libs/form-functions.js
@@ -2,6 +2,7 @@
 const selectFirstField = () => $('input[name="first_name"]').select();
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+// reference: Bootstrap
 const validateForm = () => {
   'use strict';
   window.addEventListener('load', function () {
@@ -20,32 +21,34 @@ const validateForm = () => {
   }, false);
 };
 
+// when raw form data is returned, all option fields have the same name.
+// in order to distinguish each option fields (e.g. 1st option, 2nd option, etc)
+// add a number at the end of the fields. For instance, option-1's fields are called
+// name-1, start_time-1, end_time-1, and note-1 as opposed to *-2
+function objectifyForm(formArray) {
+  const optionFields = ['name', 'start_time', 'end_time', 'note'];
+  var returnArray = {};
+  let optionNum = 0;
+
+  for (let i = 0; i < formArray.length; i++) {
+    const { name, value } = formArray[i];
+    const optionIndex = optionFields.indexOf(name);
+    if (optionIndex !== -1) {
+      if (optionIndex === 0) optionNum++;
+      returnArray[`${name}-${optionNum}`] = value;
+    } else {
+      returnArray[name] = value;
+    }
+  }
+  return returnArray;
+}
+
 // submit event form
 const submitEventForm = () => {
   $('#event-form').on('submit', function (event) {
     event.preventDefault();
+    // serialise the form data
     const formFields = $(this).serializeArray();
-
-    function objectifyForm(formArray) {//serialize data function
-      const optionFields = ['name', 'start_time', 'end_time', 'note'];
-      var returnArray = {};
-      let optionNum = 0;
-
-      for (let i = 0; i < formArray.length; i++) {
-        const name = formArray[i].name;
-        const value = formArray[i].value;
-
-        const optionIndex = optionFields.indexOf(name);
-        if (optionIndex !== -1) {
-          if (optionIndex === 0) optionNum++;
-          returnArray[`${name}-${optionNum}`] = value;
-        } else {
-          returnArray[name] = value;
-        }
-      }
-      return returnArray;
-    }
-
     const formJSON = JSON.stringify(objectifyForm(formFields));
 
     $.ajax({
@@ -55,11 +58,9 @@ const submitEventForm = () => {
       dataType: 'json',
       success: urls => {
         window.location = `${window.location.href}/${urls.admin_url}`;
-      },
-      error: {
-
       }
     })
+
   })
 };
 
