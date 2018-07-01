ec476cbc2591c09adea93aa48482aa0ace4ebb9b
byeong0430 <byeong.kim0430@gmail.com>
2018-07-01 00:16:39 +0000

    removed commented out block of code and added selectFirstField() function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,6 @@
 $(() => {
   const { activateCopyBtn, activateDeleteBtn, makeUrlCopyBtn } = activateBtns;
-
-  // activate form validator
-  validateForm();
+  const { selectFirstField, validateForm, submitEventForm } = formFunc;
   // change all icon cursors to pointer
   $('i.material-icons').css('cursor', 'pointer');
   // activate copy and delete button for options
@@ -12,74 +10,22 @@ $(() => {
   makeUrlCopyBtn();
 
 
-  // let editable = false;
-  // $('input[value="PREVIEW"]').click(function (e) {
-  //   e.preventDefault();
+  // auto focus input[name="first_name"]
+  selectFirstField();
+  // activate form validator
+  validateForm();
+  // submit event form
+  submitEventForm();
 
-  //   // remove all default icons
-  //   $(".material-icons").toggle();
-  //   // display checkbox
-  //   $(".right-icon input").toggle();
-  //   // display vote counter
-  //   $("#counter").toggle();
-  //   // display thumbs up icon
-  //   $(".fas.fa-thumbs-up").toggle();
-
-  //   $('input[value="SUBMIT"]').slideToggle("fast");
-  //   if (editable === false) {
-  //     $('.form-control').attr('readonly', true);
-  //     editable = true;
-  //   } else {
-  //     $('.form-control').attr('readonly', false);
-  //     editable = false;
-  //   };
-  // })
-
-
-
-
-
-
-  $('#event-form').on('submit', function (event) {
-    event.preventDefault();
-    const formFields = $(this).serializeArray();
-
-    function objectifyForm(formArray) {//serialize data function
-      const optionFields = ['name', 'start_time', 'end_time', 'note'];
-      var returnArray = {};
-      let optionNum = 0;
-
-      for (let i = 0; i < formArray.length; i++) {
-        const name = formArray[i].name;
-        const value = formArray[i].value;
-
-        const optionIndex = optionFields.indexOf(name);
-        if (optionIndex !== -1) {
-          if (optionIndex === 0) optionNum++;
-          returnArray[`${name}-${optionNum}`] = value;
-        } else {
-          returnArray[name] = value;
-        }
-      }
-      return returnArray;
-    }
-
-    const formJSON = JSON.stringify(objectifyForm(formFields));
-
-    $.ajax({
-      url: '/events',
-      method: 'POST',
-      data: formJSON,
-      dataType: 'json',
-      success: urls => {
-        window.location = `${window.location.href}/${urls.admin_url}`;
-      },
-      error: {
-
-      }
-    })
-  })
-
+  const handler = function(){}
+  $('input[name="name"]').blur(function () {
+    // option section
+    const $optionSection = $('.option');
+    // each option div
+    const $eachOption = $(this).parents().eq(2);
+    // $eachOption.clone(true, true).appendTo($optionSection);
+    // $eachOption.unbind();
+  });
 
 
 });
