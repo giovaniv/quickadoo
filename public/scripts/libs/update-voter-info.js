36098ab423bf55d14350705ca9cedf794229d85e
byeong0430 <byeong.kim0430@gmail.com>
2018-07-02 05:01:24 +0000

    fix: voter checkboxes were not updated

--- public/scripts/libs/update-voter-info.js
+++ public/scripts/libs/update-voter-info.js
@@ -1,14 +1,16 @@
 const displayVoterChoices = (response, $userEmail) => {
   const { user, options } = response;
   const { first_name, last_name } = user;
+
   // update the voter's first & last name input fields
   $('input[name="voter_first_name"]').val(first_name);
   $('input[name="voter_last_name"]').val(last_name);
   // also update voter's choice(s)
   $('input[type=checkbox]').each(function () {
     for (let i = 0; i < options.length; i++) {
-      if ($userEmail == options[i])
-        $userEmail.attr('checked', true);
+      if ($(this).val() == options[i]){
+        $(this).attr('checked', true);
+      }
     }
   });
 };
@@ -16,10 +18,10 @@ const displayVoterChoices = (response, $userEmail) => {
 const updateVoterInfo = () => {
   // when voter email's filled, execute the function below
   $('input[name="voter_email"]').blur(function () {
-    const email = $(this).val();
+    const $email = $(this).val();
     $.ajax('/voters', {
       method: 'POST',
-      data: { email }
+      data: { email: $email }
     }).done(res => displayVoterChoices(res, $email))
   });
 };
